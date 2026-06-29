57c0aecf7f1f095da7a52a5f191e32c3baa00f54
tantn <tantn@snine.vn>
2026-06-29 11:28:45 +0700

    test

--- movie_app/admin.py
+++ movie_app/admin.py
@@ -1,7 +1,18 @@
 from django.contrib import admin
-from .models import Movie
+from .models import Movie, Genre
 
 @admin.register(Movie)
 class MovieAdmin(admin.ModelAdmin):
-    list_display = ['title', 'release_year', 'created_at']
+    list_display = ['title', 'release_year', 'display_genres', 'created_at']
     search_fields = ['title']
+    filter_horizontal = ['genres']
+
+    def display_genres(self, obj):
+        return ", ".join(genre.name for genre in obj.genres.all())
+    display_genres.short_description = "Thể loại"
+
+
+@admin.register(Genre)
+class GenreAdmin(admin.ModelAdmin):
+    list_display = ['name']
+    search_fields = ['name']
